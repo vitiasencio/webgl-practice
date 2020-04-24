4460e8290b8a805b524d957c29b096321a0ef930
Victor Asencio <victor.x.asencio.-nd@disney.com>
2020-04-24 01:01:01 -0300

    colors per face added

--- index.js
+++ index.js
@@ -42,38 +42,103 @@ let program = createProgram(gl, vertexShader, fragmentShader);
  */
 
 const positions = [ 0.0, -0.1,    // Indice 0
-                    0.0, 0.5,     // Indice 1
                     0.2, 0.1,     // Indice 2
+                    0.0, 0.5,     // Indice 1
+                    
+                    0.0, -0.1,
                     0.6, 0.0,     // Indice 3
+                    0.2, 0.1,     // Indice 3
+                    
+                    0.0, -0.1,
                     0.35, -0.25,  // Indice 4
+                    0.6, 0.0,
+                    
+                    0.0, -0.1,
                     0.45, -0.6,   // Indice 5
-                    0.0, -0.45,   // Indice 6
-                    -0.45, -0.6,  // Indice 7
+                    0.35, -0.25,   // Indice 6
+                    
+                    0.0, -0.1,  // Indice 7
+                    0.0, -0.45, // Indice 8
+                    0.45, -0.6,    // Indice 9
+                    
+                    0.0, -0.1,  // Indice 7
+                    -0.45, -0.6, // Indice 8
+                    0.0, -0.45,    // Indice 9
+                    
+                    0.0, -0.1,  // Indice 7
                     -0.35, -0.25, // Indice 8
+                    -0.45, -0.6,    // Indice 9
+                    
+                    0.0, -0.1,  // Indice 7
+                    -0.6, 0.0, // Indice 8
+                    -0.35, -0.25,    // Indice 9
+                    
+                    0.0, -0.1,  // Indice 7
+                    -0.2, 0.1, // Indice 8
                     -0.6, 0.0,    // Indice 9
-                    -0.2, 0.1,    // Indice 10
+                    
+                    0.0, -0.1,  // Indice 7
+                    0.0, 0.5, // Indice 8
+                    -0.2, 0.1,    // Indice 9
+                    
                   ],
-      colors    = [ 22.0/256, 164.0/256, 228.0/256,
-                    0.0, 251.0/256, 220.0/256,
+      colors    = [ 0.0, 0.5, 0.5,
+                    0.0, 0.5, 0.5,
+                    0.0, 0.5, 0.5,
+
+                    0.0, 0.0, 0.5,
+                    0.0, 0.0, 0.5,
+                    0.0, 0.0, 0.5,
+        
                     0.0, 0.0, 1.0,
+                    0.0, 0.0, 1.0,
+                    0.0, 0.0, 1.0,
+        
                     0.5, 0.0, 1.0,
+                    0.5, 0.0, 1.0,
+                    0.5, 0.0, 1.0,
+        
+                    0.5, 0.3, 1.0,
+                    0.5, 0.3, 1.0,
+                    0.5, 0.3, 1.0,
+        
+                    0.4, 0.8, 1.0,
+                    0.4, 0.8, 1.0,
+                    0.4, 0.8, 1.0,
+        
+                    0.2, 1.0, 0.2,
+                    0.2, 1.0, 0.2,
+                    0.2, 1.0, 0.2,
+        
+                    0.3, 0.0, 0.0,
+                    0.3, 0.0, 0.0,
+                    0.3, 0.0, 0.0,
+        
+                    0.5, 0.5, 1.0,
+                    0.5, 0.5, 1.0,
+                    0.5, 0.5, 1.0,
+        
+                    0.5, 0.8, 0.2,
+                    0.5, 0.8, 0.2,
+                    0.5, 0.8, 0.2,
+        
                 ];
 
 
-const indexes = [ 0, 2, 1, // Triangulo 1
-                  0, 3, 2, // Triangulo 2
-                  0, 4, 3, // Triangulo 2
-                  0, 5, 4, // Triangulo 2
-                  0, 6, 5, // Triangulo 2
-                  0, 7, 6, // Triangulo 2
-                  0, 8, 7, // Triangulo 2
-                  0, 9, 8, // Triangulo 2
-                  0, 10, 9, // Triangulo 2
-                  0, 1, 10, // Triangulo 2
+const indexes = [ 0, 1, 2, // Triangulo 1
+                  3, 4, 5, // Triangulo 2
+                  6, 7, 8, // Triangulo 2
+                  9, 10, 11, // Triangulo 2
+                  12, 13, 14, // Triangulo 2
+                  15, 16, 17, // Triangulo 2
+                  18, 19, 20, // Triangulo 2
+                  21, 22, 23, // Triangulo 2
+                  24, 25, 26, // Triangulo 2
+                  27, 28, 29, // Triangulo 2
                 ];
 
 let positionBuffer = createVertexBuffer(gl, positions);
-//let colorBuffer = createVertexBuffer(gl, colors);
+let colorBuffer = createVertexBuffer(gl, colors);
 let indexBuffer = createIndexBuffer(gl, indexes);
 
 /**
@@ -81,7 +146,7 @@ let indexBuffer = createIndexBuffer(gl, indexes);
  */
 
 let positionAttributeLocation = gl.getAttribLocation(program, 'a_position');
-//let colorAttributeLocation = gl.getAttribLocation(program, 'vertex_color');
+let colorAttributeLocation = gl.getAttribLocation(program, 'vertex_color');
 
 /**
  * PASO 7 - Creo un VAO y lo bindeo
@@ -98,8 +163,8 @@ gl.bindVertexArray(vao);
 gl.enableVertexAttribArray(positionAttributeLocation);
 bindAttributeToVertexBuffer(gl, positionAttributeLocation, 2, positionBuffer, vertexSize, 0);
 
-//gl.enableVertexAttribArray(colorAttributeLocation);
-//bindAttributeToVertexBuffer(gl,colorAttributeLocation, 3, colorBuffer, 0, 0)
+gl.enableVertexAttribArray(colorAttributeLocation);
+bindAttributeToVertexBuffer(gl,colorAttributeLocation, 3, colorBuffer, 0, 0)
 
 /**
  * Conectamos el arreglo de indices que vamos a usar.
