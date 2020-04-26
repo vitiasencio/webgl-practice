953aafe4efe1ff4bc28b71399f5f481f96a79366
Victor Asencio <victor.x.asencio.-nd@disney.com>
2020-04-26 18:23:48 -0300

    two triangles with indices

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { vertexShaderSource, fragmentShaderSource } from './js/shaders.js';
-import { getCanvas, getRenderingContext, createShader, createProgram, createVertexBuffer, bindAttributeToVertexBuffer } from './js/utils.js';
+import { getCanvas, getRenderingContext, createShader, createProgram, createVertexBuffer, createIndexBuffer, bindAttributeToVertexBuffer } from './js/utils.js';
 
 const CANVASID = 'myCanvas';
 
@@ -41,19 +41,46 @@ let program = createProgram(gl, vertexShader, fragmentShader);
  * PASO 5 - Creo un buffer para las posiciones de los vertices.
  */
 
-const positions = [ -0.5, -0.5,
-                    0.5, -0.5,
-                    0.0, 0.3,
-                  ],
-      vertexCount = 3;
+const positions = [ 
+    0.0, 0.0,
+    1.0, 0.0,
+    0.0, 0.5,
+];
+
+const colors = [
+    1.0, 0.0, 0.0,
+    1.0, 0.0, 0.0,
+    1.0, 0.0, 0.0,
+]
+
+const positions2 = [ 
+    0.0, 0.0,
+    0.0, 0.5,
+    -1.0, 0.0,
+];
+
+const colors2 = [
+    0.0, 1.0, 0.0,
+    0.0, 1.0, 0.0,
+    0.0, 1.0, 0.0,
+]
+
+const indices = [
+    0, 1, 2,
+]
 
 let positionBuffer = createVertexBuffer(gl, positions);
+let position2Buffer = createVertexBuffer(gl, positions2);
+let colorBuffer = createVertexBuffer(gl, colors)
+let color2Buffer = createVertexBuffer(gl, colors2)
+let indexBuffer = createIndexBuffer(gl, indices);
 
 /**
  * PASO 6 - Obtengo la posicion del atributo de entrada al vertex shader
  */
 
 let positionAttributeLocation = gl.getAttribLocation(program, 'a_position');
+let colorAttributeLocation = gl.getAttribLocation(program, 'vertex_color');
 
 /**
  * PASO 7 - Creo un VAO y lo bindeo
@@ -68,10 +95,32 @@ gl.bindVertexArray(vao);
  */
 
 gl.enableVertexAttribArray(positionAttributeLocation);
-bindAttributeToVertexBuffer(gl, positionAttributeLocation, 2, positionBuffer, vertexSize, 0);
+bindAttributeToVertexBuffer(gl, positionAttributeLocation, 2, positionBuffer);
+gl.enableVertexAttribArray(colorAttributeLocation);
+bindAttributeToVertexBuffer(gl, colorAttributeLocation, 3, colorBuffer);
+
+gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
 
 gl.bindVertexArray(null);
 
+//////////////////////
+const vao2 = gl.createVertexArray();
+gl.bindVertexArray(vao2);
+
+/**
+ * PASO 8 - Habilitamos cada atributo y lo linkeamos a su buffer
+ * Dejamos de tomar nota del VAO
+ */
+
+gl.enableVertexAttribArray(positionAttributeLocation);
+bindAttributeToVertexBuffer(gl, positionAttributeLocation, 2, position2Buffer);
+gl.enableVertexAttribArray(colorAttributeLocation);
+bindAttributeToVertexBuffer(gl, colorAttributeLocation, 3, color2Buffer);
+
+gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+
+gl.bindVertexArray(null);
+//////////////////////
 /**
  * PASO 9 - Le decimos a WebGL que use el program creado con nuestros shaders
  * Bindeo el VAO.
@@ -94,5 +143,8 @@ gl.uniform4f(colorUniformLocation, 0.0, 1.0, 0.0, 1.0);*/
  * PASO 10 - Dibujo
  */
 
-gl.drawArrays(gl.TRIANGLES, 0, vertexCount);
+gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
 
+gl.bindVertexArray(vao2);
+
+gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
